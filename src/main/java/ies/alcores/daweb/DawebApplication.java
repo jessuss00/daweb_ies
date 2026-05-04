6dc079dbd2313cceec05170ecade95686406efde
vricheb973 <vricheb973@g.educaand.es>
2026-05-04 13:42:22 +0200

    Initial commit

---
+++ src/main/java/ies/alcores/daweb/DawebApplication.java
@@ -0,0 +1,13 @@
+package ies.alcores.daweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ies/alcores/daweb/DawebApplicationTests.java
@@ -0,0 +1,13 @@
+package ies.alcores.daweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
